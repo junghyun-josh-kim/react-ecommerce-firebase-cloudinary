22a9f99777f7cff57583b7a399bff773a55ea579
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2022-11-12 21:52:19 -0500

    Navbar: JSX

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsFillPencilFill } from 'react-icons/bs';
+import { FiShoppingBag } from 'react-icons/fi';
 
 export default function Navbar() {
   return (
     <header>
       <Link to='/'>
         <h1>lululime</h1>
+        <FiShoppingBag />
       </Link>
+
+      <nav>
+        <Link to='/products'>Products</Link>
+        <Link to='/carts'>My Carts</Link>
+        <Link to='/products/new'>
+          <BsFillPencilFill />
+        </Link>
+        <button>Login</button>
+      </nav>
     </header>
   );
 }
