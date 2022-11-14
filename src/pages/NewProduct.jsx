07145beb638fedbec36b2c0992ee240dc7f35237
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2022-11-13 20:35:08 -0500

    Firesbase: Upload db
    Firebase: Realtime Database

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { addNewProduct } from '../api/firebase';
 import { uploadImage } from '../api/uploader';
 import Button from '../components/ui/Button';
 
@@ -10,6 +11,7 @@ export default function NewProduct() {
     e.preventDefault();
     uploadImage(file).then((url) => {
       console.log(url);
+      addNewProduct(product, url);
     });
   };
   const handleChange = (e) => {
