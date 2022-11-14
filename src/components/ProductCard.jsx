d7db6fef30a23b073971b8664eca32b65b5ef226
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2022-11-13 22:43:40 -0500

    ProductCard: Tailwind CSS

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,16 +4,17 @@ export default function ProductCard({
   product: { id, image, title, price, category },
 }) {
   return (
-    <li>
+    <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
       <img
+        className='w-full'
         src={image}
         alt={title}
       />
-      <div>
-        <h3>{title}</h3>
+      <div className='mt-2 px-2 text-lg'>
+        <h3 className='truncate'>{title}</h3>
         <p>{`CAD $ ${price}`}</p>
       </div>
-      <p>{category}</p>
+      <p className='mb-2 px-2 text-gray-600'>{category}</p>
     </li>
   );
 }
